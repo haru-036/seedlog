a0d75f1895f1a71a74f2b3ba81da9a0e1fb256e3
haru <hinagoro5656@gmail.com>
2026-03-11 14:57:17 +0900

    テキストの色を変えた

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Seedlog</h1>
+      <h1 className="text-2xl font-bold text-amber-500">Seedlog</h1>
     </div>
   );
 }
